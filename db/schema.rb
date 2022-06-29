2b0d043ebf4a3f333fe6468ab43917d17dddea36
+ <+>
2022-06-29 11:04:43 -0400

    Migrated organizations table

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_06_29_145619) do
+ActiveRecord::Schema[7.0].define(version: 2022_06_29_150404) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "organizations", force: :cascade do |t|
+    t.string "name"
+    t.string "address"
+    t.string "phone_number"
+    t.string "email"
+    t.string "logo_image_url"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "user_infos", force: :cascade do |t|
     t.integer "user_id"
