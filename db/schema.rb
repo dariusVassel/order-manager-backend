ff0916883f22e2835dc77fb1394f4f02dc20fad1
+ <+>
2022-06-29 10:57:12 -0400

    Migrated User Model

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_06_28_205418) do
+ActiveRecord::Schema[7.0].define(version: 2022_06_29_145619) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "user_infos", force: :cascade do |t|
+    t.integer "user_id"
+    t.string "first_name"
+    t.string "last_name"
+    t.string "avatar_url"
+    t.string "email"
+    t.string "company_name"
+    t.string "address"
+    t.string "phone"
+    t.integer "completed_orders"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username"
