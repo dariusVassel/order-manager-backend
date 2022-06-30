2addd99defb038d42de516cd87755ec874bac24c
samarth-gwalani <108356851+samarth-gwalani@users.noreply.github.com>
2022-06-30 11:47:47 -0400

    Adding foreign key

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,6 +1,6 @@
 class Order < ApplicationRecord
-    belongs_to :buyer, class_name: 'User'
-    belongs_to :seller, class_name: 'User'
-    belongs_to :agent, class_name: 'User'
+    belongs_to :buyer, class_name: 'User', foreign_key: :buyer_id
+    belongs_to :seller, class_name: 'User', foreign_key: :seller_id
+    belongs_to :agent, class_name: 'User', foreign_key: :agent_id
     belongs_to :order_item
 end
