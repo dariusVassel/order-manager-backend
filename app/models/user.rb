58ae9af218dc8a9ac1ffcf667665b33117343b05
samarth-gwalani <108356851+samarth-gwalani@users.noreply.github.com>
2022-07-01 12:01:13 -0400

    User signup validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,10 @@ class User < ApplicationRecord
 
     has_many :buyer_orders, class_name: "Order", foreign_key: "buyer_id"
     has_many :seller_orders, class_name: "Order", foreign_key: "seller_id"
+
+    validates :username, presence: true, length: {minimum: 2}, uniqueness: true
+    validates :password, presence: true
+
    
     # def orders
     #     Order.where("buyer_id = ? OR seller_id = ?", id, id)
