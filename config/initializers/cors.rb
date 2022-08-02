285140a83533a552ee3a43b7e5b0e506d4e9e55a
samarth-gwalani <108356851+samarth-gwalani@users.noreply.github.com>
2022-08-02 17:01:18 -0400

    cors test

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -8,6 +8,9 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins "https://order-manager-app-build.herokuapp.com/"
+    origins "*"
+
+
 
     resource "*",
       headers: :any,
