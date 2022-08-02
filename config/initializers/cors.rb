aa8dbd912ffbd8fd48bf57a3ae22e05b91371346
samarth-gwalani <108356851+samarth-gwalani@users.noreply.github.com>
2022-08-02 15:44:50 -0400

    updated cors origins

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "https://frozen-badlands-72757.herokuapp.com/"
+    origins "frozen-badlands-72757.herokuapp.com"
 
     resource "*",
       headers: :any,
